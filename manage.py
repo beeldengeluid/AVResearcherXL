49a748b66b8b0a8cd788d1e2a891c66d9f2e8f6e
Bart de Goede <bdegoede@gmail.com>
2014-04-21 15:33:30 +0200

    Create quamerdes docs from avresearcher data

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from elasticsearch.helpers import bulk
 import json
 import logging
 from quamerdes.settings import ES_SEARCH_HOST, ES_SEARCH_PORT
+import pprint
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
 
@@ -21,6 +22,12 @@ def getAVRDoc(line):
         else:
             return doc
     return None
+
+
+def serialize_quamerdes(data):
+    doc_id = data['_meta'].get('AVRID')
+    with open('dumps/data/immix/%s.json' % doc_id, 'wb') as f:
+        json.dump(data, f)
 
 
 class LoadAVRDataToES(Command):
@@ -59,14 +66,15 @@ class LoadAVRDataToES(Command):
 
 
 class TransformAVRData(Command):
+
     """
     Transform AVResearcher expressions to QuaMeRDES documents.
     """
 
     def get_options(self):
         return (
-                Option('-d', '--dump', default='dumps/avresearcher.json',
-                        dest='avrdump', ),
+            Option('-d', '--dump', default='dumps/avresearcher.json',
+                   dest='avrdump', ),
         )
 
     def run(self, avrdump):
@@ -75,21 +83,63 @@ class TransformAVRData(Command):
             for line in f:
                 doc = getAVRDoc(line)
                 if doc:
-                    data = {
-                        'date': doc['_source']['immixDate']
-                    }
-                    if not doc.get('_source', {}).get('immixDate', None):
-                        print doc
-                        # print json.dumps(doc)
-                        # print '=' * 80
+                    source = doc.get('_source', {})
 
+                    data = {
+                        'title': source.get('mainTitle', None),
+                        'source': 'http://zoeken.beeldengeluid.nl/internet/in'
+                                  'dex.aspx?chapterid=1164&contentid=7&verity'
+                                  'ID=%s@expressies' % doc.get('_id')
+                    }
+
+                    broadcastdates = sorted(source.get('broadcastDates', []),
+                                            key=lambda d: d['start'])
+
+                    if broadcastdates:
+                        data['date'] = broadcastdates[0]['start']
+                    else:
+                        data['date'] = None
+
+                    data['text'] = u' '.join([u' '.join(source.get(field, u''))
+                                             for field in ['titles', 'mainTitle', 'summaries', 'descriptions']])
+                    data['_meta'] = {
+                        'broadcasters': source.get('broadcasters', []),
+                        'broadcastdates': broadcastdates,
+                        'categories': [{
+                            'key': category.get('categoryKey'),
+                            'value': category.get('categoryValue')
+                        } for category in source.get('categories', [])],
+                        'descriptions': source.get('descriptions', []),
+                        'expressieID': source.get('expressieId'),
+                        # Not entirely sure what this is; maybe the date the
+                        # expression was added to iMMix?
+                        'immixDate': source.get('immixDate'),
+                        'levelTitles': [{
+                            'title': title,
+                            'level': level
+                        } for level, title in source.get(
+                            'levelTitles', {}).items()],
+                        'mainTitle': source.get('mainTitle'),
+                        'realisatieID': source.get('realisatieId'),
+                        'reeksID': source.get('reeksId'),
+                        'roles': [{
+                            'key': role.get('roleKey'),
+                            'value': role.get('roleValue'),
+                            'playerName': role.get('rolePlayerName'),
+                            'playerFunction': role.get('rolePlayerFunction')
+                        } for role in source.get('roles', [])],
+                        'subtitles': source.get('subtitles', []),
+                        'summaries': source.get('summaries', []),
+                        'titles': source.get('titles', []),
+                        'werkID': source.get('werkID'),
+                        'AVRID': doc.get('_id')
+                    }
+
+                    serialize_quamerdes(data)
 
                 i += 1
                 if i % 1000 == 0:
-                    print 'Checked', i, 'documents'
-                # if i > 2:
-                #     break
-
+                    print 'Processed', i, 'documents'
 
 
 manager = Manager(app)
