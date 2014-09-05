383ecca6a86042b51fbfb53c87fe9e697237ac51
Justin van Wees <justin@vwees.net>
2014-09-05 15:54:31 +0200

    Display a warning when no results were found

--- quamerdes/static/js/views/search/query_input.js
+++ quamerdes/static/js/views/search/query_input.js
@@ -19,7 +19,8 @@ function($, _, Backbone, app, FieldsView, queryInputTemplate){
         initialize: function(options){
             this.fields = new FieldsView({ model: this.model });
 
-            this.model.on('change:loading', this.toggleLoadingIndicator, this);;
+            this.model.on('change:loading', this.toggleLoadingIndicator, this);
+            this.model.on('change:loading', this.showNoResultsWarning, this);
         },
 
         render: function(){
@@ -31,7 +32,7 @@ function($, _, Backbone, app, FieldsView, queryInputTemplate){
             e.preventDefault();
             var querystring = this.$('input.query').val().trim();
 
-            // Also log empty querystrings
+            // Also log empty query strings
             app.vent.trigger('Logging:clicks', {
                 action: 'submit_query',
                 modelName: this.model.get('name'),
@@ -46,6 +47,9 @@ function($, _, Backbone, app, FieldsView, queryInputTemplate){
             if(!querystring){
                 return;
             }
+
+            // Remove the 'zero results warning' if it is still visible
+            this.$el.find('.no-results').remove();
 
             this.model.freeTextQuery(querystring);
         },
@@ -80,6 +84,20 @@ function($, _, Backbone, app, FieldsView, queryInputTemplate){
                 this.$el.find('.loading').show();
             } else {
                 this.$el.find('.loading').hide();
+            }
+        },
+
+        showNoResultsWarning: function() {
+            if (this.model.get('loading') === false && this.model.get('totalHits') === 0) {
+                var warning = $('<div class="no-results alert alert-warning"><strong>No results!</strong> There are no documents in the collection that match your query.</div>');
+                this.$el.append(warning);
+                warning.fadeIn('fast', function() {
+                    setTimeout(function() {
+                        warning.fadeOut('fast', function() {
+                            warning.remove();
+                        });
+                    }, 1700);
+                });
             }
         }
     });
